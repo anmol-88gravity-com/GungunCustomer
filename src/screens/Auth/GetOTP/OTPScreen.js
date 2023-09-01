d22152b9627a41e29fcc9b28f8bbf2fd151a1511
priyapal88 <priyapal@88gravity.com>
2023-09-01 18:25:22 +0530

    OTP_Screen

--- src/screens/Auth/GetOTP/OTPScreen.js
+++ src/screens/Auth/GetOTP/OTPScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import {
   SafeAreaView,
   View,
@@ -10,41 +10,45 @@ import {
   Platform,
 } from 'react-native';
 
-import {images} from '../../../utils/Images';
-import {styles} from './OTPScreen.styles';
+import { images } from '../../../utils/Images';
+import { styles } from './OTPScreen.styles';
 
-export const OTPScreen = ({navigation}) => {
+export const OTPScreen = ({ navigation }) => {
   const [code, setCode] = useState('');
-  const [phone, setPhone] = useState('');
+  const inputRefs = useRef([]);
 
-  const inputRef1 = useRef(null);
-  const inputRef2 = useRef(null);
-  const inputRef3 = useRef(null);
-  const inputRef4 = useRef(null);
-  const inputRef5 = useRef(null);
+  const handleCodeChange = (value, index) => {
+    const tempCode = code.split('');
+    tempCode[index] = value;
+    setCode(tempCode.join(''));
 
-  const handleCodeChange = (value, inputRef) => {
-    setCode(value);
-    if (value.length === 1) {
-      inputRef.current.focus();
+    if (value.length === 1 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
     }
   };
+
+
+  const handleVerify = () => {
+    setCode('');
+    inputRefs.current.forEach((ref) => ref.clear());
+  };
+
 
   const borderBottom =
     Platform.OS === 'ios'
       ? {
-          borderBottomColor: code.length > 0 ? '#000' : '#ccc',
-          borderBottomWidth: 2,
-        }
+        borderBottomColor: code.length > 0 ? '#000' : '#ccc',
+        borderBottomWidth: 2,
+      }
       : {
-          borderBottomColor: code.length > 0 ? '#000' : '#ccc',
-          borderWidth: 0,
-          borderBottomWidth: 2,
-        };
+        borderBottomColor: code.length > 0 ? '#000' : '#ccc',
+        borderWidth: 0,
+        borderBottomWidth: 2,
+      };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <ImageBackground
           source={images.backgroundImg}
           style={styles.backgroundImage}>
@@ -56,54 +60,30 @@ export const OTPScreen = ({navigation}) => {
       </View>
       <View style={styles.loginView}>
         <ScrollView>
-          <View style={{marginHorizontal: 20}}>
-            <View style={{marginTop: '15%'}}>
+          <View style={{ marginHorizontal: 20 }}>
+            <View style={{ marginTop: '15%' }}>
               <Text style={styles.headingText}>Verify OTP to Continue</Text>
             </View>
 
             <View style={styles.inputTextField}>
-              <TextInput
-                style={[styles.inputText, borderBottom]}
-                maxLength={1}
-                keyboardType="numeric"
-                value={code[0]}
-                onChangeText={value => handleCodeChange(value, inputRef2)}
-                ref={inputRef1}
-              />
-              <TextInput
-                style={[styles.inputText, borderBottom]}
-                maxLength={1}
-                keyboardType="numeric"
-                // placeholder="."
-                value={code[1]}
-                onChangeText={value => handleCodeChange(value, inputRef3)}
-                ref={inputRef2}
-              />
-              <TextInput
-                style={[styles.inputText, borderBottom]}
-                maxLength={1}
-                keyboardType="numeric"
-                // placeholder="."
-                value={code[2]}
-                onChangeText={value => handleCodeChange(value, inputRef4)}
-                ref={inputRef3}
-              />
-              <TextInput
-                style={[styles.inputText, borderBottom]}
-                maxLength={1}
-                keyboardType="numeric"
-                // placeholder="."
-                value={code[3]}
-                onChangeText={value => handleCodeChange(value, inputRef5)}
-                ref={inputRef4}
-              />
+              {[0, 1, 2, 3].map((index) => (
+                <TextInput
+                  key={index}
+                  style={[styles.inputText, borderBottom]}
+                  maxLength={1}
+                  keyboardType="numeric"
+                  value={code[index]}
+                  ref={(ref) => (inputRefs.current[index] = ref)}
+                  onChangeText={(value) => handleCodeChange(value, index)}
+                />
+              ))}
             </View>
 
-            <TouchableOpacity style={styles.btnView}>
+            <TouchableOpacity style={styles.btnView} >
               <Text style={styles.textSignIn}>Verify</Text>
             </TouchableOpacity>
 
-            <View style={{marginTop: '5%'}}>
+            <View style={{ marginTop: '5%' }}>
               <Text style={styles.verifyOTPText}>Didn't Received the OTP?</Text>
               <Text
                 style={styles.btnResend}
