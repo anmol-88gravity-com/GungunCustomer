5ae002630fabb1fab6f4dfda34a9219efcdfa047
priyapal88 <priyapal@88gravity.com>
2023-09-05 12:06:01 +0530

    noChanges

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -8,6 +8,7 @@ export default function (state = initial_state, action) {
     switch (action.type) {
         case types.LOGIN:
             const data = action.payload
+            console.log('userDataReducer--',data)
             return { userData: data }
         default:
            return{...state};
